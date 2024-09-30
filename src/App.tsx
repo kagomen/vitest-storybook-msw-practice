e51693c13942c2a3c1a6f2a494b7c3b7516773e4
kagome <fu.taz.1102@gmail.com>
2024-09-30 12:40:21 +0900

    add storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+
+type Option = {
+  value: string;
+  label: string;
+};
+
+const defaultOptions = [
+  { value: "1", label: "Option 1" },
+  { value: "2", label: "Option 2" },
+  { value: "3", label: "Option 3" },
+];
 
 function App() {
-  return <></>;
+  const [options, setOptions] = useState<Option[]>(defaultOptions);
+
+  useEffect(() => {
+    fetch("/api/options")
+      .then((res) => res.json())
+      .then((data) => setOptions(data));
+  }, []);
+
+  return (
+    <form>
+      <select>
+        {options.map((option) => (
+          <option value={option.value}>{option.label}</option>
+        ))}
+      </select>
+    </form>
+  );
 }
 
 export default App;
